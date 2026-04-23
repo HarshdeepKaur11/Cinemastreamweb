19a7647599ff1425487dd3feec6cfc634ea235ea
Harshdeep kaur <harschx31@gmail.com>
2026-04-23 10:37:05 +0530

    ready for deployment

--- cinemastreamweb/settings.py
+++ cinemastreamweb/settings.py
@@ -33,6 +33,9 @@ INSTALLED_APPS = [
     'admin_panel',
 ]
 
+ALLOWED_HOSTS = ['*']
+
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -41,6 +44,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'cinemastreamweb.urls'
