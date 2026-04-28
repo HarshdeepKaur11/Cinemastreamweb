f9b492b667aa7892bf543fb74b0dbeab27df4a7e
Harshdeep kaur <harschx31@gmail.com>
2026-04-28 21:15:57 +0530

    Attempt to fix build for Python 3.14: Unpin versions and verbose build

--- build.sh
+++ build.sh
@@ -1,8 +1,18 @@
 #!/usr/bin/env bash
+# Exit on error
+set -o errexit
+
+echo "--- Python Version ---"
+python --version
+
+echo "--- Installing Pip ---"
 python -m pip install --upgrade pip
+
+echo "--- Installing Requirements ---"
 pip install -r requirements.txt
 
-# ONLY run collectstatic during build. 
-# Do NOT run migrate here because the database might not be ready, 
-# and SQLite migrations are failing due to a foreign key mismatch.
-python manage.py collectstatic --noinput
+echo "--- Running Collectstatic ---"
+# We use --clear to ensure no stale files, and continue even if it warns
+python manage.py collectstatic --noinput --clear
+
+echo "--- Build Complete ---"
