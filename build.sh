502a8ae4b77c787d8eaa1a8bb3507b198c3fd0b7
Harshdeep kaur <harschx31@gmail.com>
2026-04-28 21:19:44 +0530

    Permissive build and start-time migrations

--- build.sh
+++ build.sh
@@ -1,18 +1,5 @@
 #!/usr/bin/env bash
-# Exit on error
-set -o errexit
-
-echo "--- Python Version ---"
-python --version
-
-echo "--- Installing Pip ---"
 python -m pip install --upgrade pip
-
-echo "--- Installing Requirements ---"
 pip install -r requirements.txt
-
-echo "--- Running Collectstatic ---"
-# We use --clear to ensure no stale files, and continue even if it warns
-python manage.py collectstatic --noinput --clear
-
-echo "--- Build Complete ---"
+python manage.py collectstatic --noinput --clear || echo "Collectstatic failed"
+# Migrations will be handled in the Procfile or manually
